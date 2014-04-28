dbb7e92499a241ab86bebbb92c394a4dc15f58e6
Sunil <sunil.gandhi007@gmail.com>
2014-04-28 08:30:02 -0400

    reordered the code

--- Processor.py
+++ Processor.py
@@ -8,8 +8,21 @@ class Tests(unittest.TestCase):
 class Pipeline:
     #get all parameters from file and initialize particular pipeline
     def __init__(self):
-        pass
+        self.read_files()
 
+        # Create All Units
+        self.IF = Unit('IF',1,True)
+        self.ID = Unit('ID',1,True)
+        self.EXMULT = Unit('EXMULT',6,True)
+        self.EXADD = Unit('EXADD',4,True)
+        self.WB = Unit('WB',1,True)
+    
+    def read_files(self):
+        f = open('inst.txt')
+        self.set_of_instructions = f.readlines()
+        f.close()
+
+    
     def __str__(self):
         pipeline_state=""
         pipeline_state += self.IF.__str__()
@@ -19,16 +32,13 @@ class Pipeline:
         return pipeline_state
 
     def update_pipeline(self):
-        # Create All Units
-        self.IF = Unit('IF',1,True)
-        self.ID = Unit('ID',1,True)
-        self.EXMULT = Unit('EXMULT',6,True)
-        self.EXADD = Unit('EXADD',4,True)
-        self.WB = Unit('WB',1,True)
         
-        self.IF.add_new_inst('GG: L.D F1, 4(R4)',0)
-        
-        for i in range(1,11):
+        current_inst = 0
+        self.IF.add_new_inst(self.set_of_instructions[current_inst].strip(),0)
+        current_inst +=1
+
+        #while(1):
+        for i in range(1,100):
             print "-------------"+str(i)+"--------------"
             print self
             last_stage_instruction = self.WB.last_stage.instruction
@@ -47,12 +57,18 @@ class Pipeline:
             self.ID.update_unit(i)
             if(self.ID.is_free() and not self.IF.is_free()):
                 self.ID.add_inst(self.IF,i)
+
             self.IF.update_unit(i)
-            
-
-
+            if(self.IF.is_free()):
+                self.IF.add_new_inst(self.set_of_instructions[current_inst].strip(),i)
+                current_inst +=1
+                
+            if(current_inst==len(self.set_of_instructions)):
+                break
+                
 
 if __name__ == '__main__':
     #unittest.main()
     p1 = Pipeline()
     p1.update_pipeline()
+    
