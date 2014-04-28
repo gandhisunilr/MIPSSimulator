953e014a9ee33007da21897c9e2991429a23ea5c
Sunil <sunil.gandhi007@gmail.com>
2014-04-27 21:01:19 -0400

    Implemented pipeline object skeleton

--- Processor.py
+++ Processor.py
@@ -10,19 +10,37 @@ class Pipeline:
     def __init__(self):
         pass
 
-    def update_pipeline():
-        IF = Unit('IF',1,True)
-        IF.add_new_inst('GG: L.D F1, 4(R4)',0)
-        EXADD = Unit('EXADD',6,True)
+    def __str__(self):
+        pipeline_state=""
+        pipeline_state += self.IF.__str__()
+        pipeline_state += self.EXADD.__str__()
+        return pipeline_state
+
+    def update_pipeline(self):
+        # Create All Units
+        self.IF = Unit('IF',1,True)
+        self.ID = Unit('ID',1,True)
+        self.EXMULT = Unit('EXMULT',6,True)
+        self.EXADD = Unit('EXADD',4,True)
+        self.WB = Unit('WB',1,True)
+        
+        self.IF.add_new_inst('GG: L.D F1, 4(R4)',0)
+        
         for i in range(1,11):
             print "-------------"+str(i)+"--------------"
-        #Find whether there is instruction in last stage
-            print IF
-            print EXADD
-            last_stage_instruction = EXADD.last_stage.instruction
-            EXADD.update_unit(i)
-            if(EXADD.is_last_stage_free() and last_stage_instruction!=None):
+            last_stage_instruction = self.EXADD.last_stage.instruction
+            # Find whether there is instruction in last stage
+            print self
+            self.WB.update_unit(i)
+            
+            self.EXADD.update_unit(i)
+            if(self.EXADD.is_last_stage_free() and last_stage_instruction!=None):
                 print last_stage_instruction.operation+" completed at "+str(i)
-                if(EXADD.is_free() and not IF.is_free()):
-                    EXADD.add_inst(IF,i)
-                    IF.update_unit(i)
+            if(self.EXADD.is_free() and not self.IF.is_free()):
+                self.EXADD.add_inst(self.IF,i)
+                self.IF.update_unit(i)
+
+if __name__ == '__main__':
+    #unittest.main()
+    p1 = Pipeline()
+    p1.update_pipeline()
